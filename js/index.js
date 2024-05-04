7263e3f939a48874990f05864a6d2ad5fe960468
Muhammad Abrar <legendabrar44@gmail.com>
2024-05-04 23:16:18 +0500

    menu finalised thought
    I have just thought that it had been complete as i have centered div add js to popup Nd show oh animation remaining but thay will be at last also I gave z-index to menu icon so that it popup sue to absolutw position

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var nameVar = document.querySelector('#name');
 var authorVar = document.querySelector('#author');
-
+var menuVar = document.querySelector('#menu');
 function truncate(string, limit){
   if (nameVar.innerHTML.length <= limit){
     return string;
@@ -14,3 +14,14 @@ var nameVars = document.querySelectorAll('#name');
 for (var a=0; a<=nameVars.length-1; a++) {
   nameVars[a].innerHTML = (truncate(nameVar.innerHTML,10));
 }
+
+menuVar.addEventListener('click', function(){
+  var menu = document.querySelector('.menu');
+  if(menu.style.display === 'block'){
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+    menuVar.style.position = 'absolute';
+    menuVar.style.zIndex = '100';
+  }
+});
